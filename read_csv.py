37e1190dc0d4deb2c33a66778f34f8369ef8c2e4
Mega-Barrel <saurabhjoshi24business@gmail.com>
2021-02-13 23:12:07 +0530

    Code for Reading and storing data

---
+++ read_csv.py
@@ -0,0 +1,40 @@
+
+import pandas as pd
+import sqlalchemy
+
+
+# Creating connecting object
+
+engine = sqlalchemy.create_engine('mysql+pymysql://saurabh:saurabh_sql@localhost:3306/UC_Davis')
+
+
+# Reading the CSV files
+
+print('Reading noc_regions CSV file')
+regions = pd.read_csv('Dataset/noc_regions.csv')
+
+
+print('Reading athlete_events CSV file')
+events = pd.read_csv('Dataset/athlete_events.csv')
+
+
+'''
+Using inbuild pandas function to convert CSV to MySQL
+'''
+
+events.to_sql(
+    'athlete_events',
+    con = engine,
+    index = False,
+    if_exists = 'append'
+)
+print('Finished storing CSV file to MySQL database!')
+
+
+regions.to_sql(
+    'regions',
+    con = engine,
+    index = False,
+    if_exists = 'append'
+)
+print('Finished storing CSV file to MySQL database!')
