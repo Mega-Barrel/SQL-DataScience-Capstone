2160ca8f1ac29b01efc2af66ab63705e9df97d76
Mega-Barrel <saurabhjoshi24business@gmail.com>
2021-02-23 21:57:16 +0530

    More changes to be Done...

--- Scripts/insert_data.sql
+++ Scripts/insert_data.sql
@@ -1,5 +1,78 @@
 USE uc_davis;
 
-INSERT INTO noc (noc_name)
-SELECT NOC
+-- iNSERTING RECORDS IN THE CITIES TABLE
+INSERT INTO cities (city_name)
+SELECT City
+FROM athlete_events
+GROUP BY City;
+
+
+-- INSERTING RECORDS IN THE SEASONS TABLE 
+INSERT INTO seasons (season_name)
+SELECT Season
 FROM athlete_events;
+
+
+-- INSERTING RECORDS IN THE REGIONS TABLE
+INSERT INTO regions (city_id, region_name, country_name)
+SELECT c.city_id, co.NOC, co.region
+FROM cities c, country co;
+
+-- INSERTING RECORDS IN THE GAMES TABLE
+INSERT INTO games (city_id, season_id, year)
+SELECT (
+    (SELECT city_id FROM cities),
+    (SELECT season_id FROM seasons),
+    (SELECT Year FROM athlete_events)
+);
+
+
+-- INSERTING RECORDS IN THE SPORTS TABLE
+INSERT INTO sports (season_id, sport_name)
+SELECT (
+    (SELECT season_id FROM seasons),
+    (SELECT Sport FROM athlete_events)
+);
+
+
+-- INSERTING RECORDS IN THE EVENTS TABLE
+INSERT INTO events (sport_id, event_name)
+SELECT (
+    (SELECT sport_id FROM sports),
+    (SELECT Event FROM athlete_events)
+);
+
+
+-- INSERTING RECORDS INTO REGIONS TABLE
+INSERT INTO regions (city_id, region_name, country_name)
+SELECT (
+    (SELECT city_id FROM cities),
+    (SELECT NOC, region FROM regions)
+);
+
+
+-- INSERTING RECORDS INTO TEAMS TABLE
+INSERT INTO teams (region_id, team_name)
+SELECT (
+    (SELECT region_id FROM regions),
+    (SELECT Team FROM athlete_events)
+);
+
+
+-- INSERTING DATA INTO ATHLETES TABLE
+INSERT INTO athletes (region_id, athlete_name, athlete_gender)
+SELECT (
+    (SELECT region_id FROM nocs),
+    (SELECT Name, Sex FROM athlete_events)  
+);
+
+
+-- INSERTING DATA INTO Sports_Stats TABLE
+INSERT INTO sports_stats (athlete_id, game_id, event_id, medal_id, athlete_age, athlete_height, athlete_weight)
+SELECT (
+    (SELECT athlete_id FROM athletes),
+    (SELECT game_id FROM games),
+    (SELECT event_id FROM events),
+    (SELECT medal_id FROM medals),
+    (SELECT Age, Height, Weight FROM athlete_events)
+);
--- Scripts/normalization.sql
+++ Scripts/normalization.sql
@@ -1,6 +1,8 @@
 -- DATABASE NAME
 USE uc_davis;
 
+DROP TABLE sports_stats, medals, seasons, cities, events, sports, games, athletes, regions, teams;
+
 -- MAIN TABLE
 CREATE TABLE IF NOT EXISTS sports_stats (
     sports_stats_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -12,17 +14,41 @@ CREATE TABLE IF NOT EXISTS sports_stats (
     athlete_height INT,
     athlete_weight INT,
     
-    FOREIGN KEY (athlete_id) REFERENCES athlete(athlete_id),
-    FOREIGN KEY (game_id) REFERENCES game(game_id),
+    FOREIGN KEY (athlete_id) REFERENCES athletes(athlete_id),
+    FOREIGN KEY (game_id) REFERENCES games(game_id),
     FOREIGN KEY (event_id) REFERENCES events(event_id),
-	FOREIGN KEY (medal_id) REFERENCES medal(medal_id) 
+	FOREIGN KEY (medal_id) REFERENCES medals(medal_id) 
 );
 
 
 -- MEDAL TABLE
 CREATE TABLE IF NOT EXISTS medals (
     medal_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    medal_type VARCHAR(20)
+    medal_type VARCHAR(100)
+);
+
+
+-- SEASONS TABLE
+CREATE TABLE IF NOT EXISTS seasons (
+    season_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    season_name VARCHAR(100)
+);
+
+
+-- CITIES TABLE
+CREATE TABLE IF NOT EXISTS cities (
+    city_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    city_name VARCHAR(100)
+);
+
+
+-- SPORTS TABLE
+CREATE TABLE IF NOT EXISTS sports (
+    sport_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    season_id INT,
+    sport_name VARCHAR(100),
+    FOREIGN KEY (season_id)
+        REFERENCES seasons (season_id)
 );
 
 
@@ -30,26 +56,16 @@ CREATE TABLE IF NOT EXISTS medals (
 CREATE TABLE IF NOT EXISTS events (
     event_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     sport_id INT,
-    event_name VARCHAR(50),
+    event_name VARCHAR(100),
     FOREIGN KEY (sport_id)
         REFERENCES sports (sport_id)
-);
-
-
--- SPORTS TABLE
-CREATE TABLE IF NOT EXISTS sports (
-    sport_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    season_id INT
-    sport_name VARCHAR(50),
-    FOREIGN KEY (season_id)
-        REFERENCES seasons (season_id)
 );
 
 
 -- GAME TABLE
 CREATE TABLE IF NOT EXISTS games (
     game_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    city_id VARCHAR(50),
+    city_id INT,
     season_id INT,
     year INT,
     FOREIGN KEY (season_id)
@@ -60,44 +76,33 @@ CREATE TABLE IF NOT EXISTS games (
 );
 
 
--- SEASONS TABLE
-CREATE TABLE IF NOT EXISTS seasons (
-    season_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    season_name VARCHAR(30)
-);
-
-
-
--- CITIES TABLE
-CREATE TABLE IF NOT EXISTS cities (
-    city_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    city_name VARCHAR(20)
+-- REGIONS TABLE
+CREATE TABLE IF NOT EXISTS regions (
+    region_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    city_id INT,
+    region_name VARCHAR(100),
+    country_name VARCHAR(100),
+    FOREIGN KEY (city_id)
+        REFERENCES cities (city_id)
 );
 
 
 -- ATHLETE TABLE
 CREATE TABLE IF NOT EXISTS athletes (
     athlete_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    noc_id INT,
-    athlete_name VARCHAR(30),
-    athlete_gender VARCHAR(10),
-    FOREIGN KEY (noc_id)
-        REFERENCES noc (noc_id)
-);
-
-
--- NOC TABLE
-CREATE TABLE IF NOT EXISTS nocs (
-    noc_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    noc_name VARCHAR(20)
+    region_id INT,
+    athlete_name VARCHAR(100),
+    athlete_gender VARCHAR(100),
+    FOREIGN KEY (region_id)
+        REFERENCES regions (region_id)
 );
 
 
 -- TEAM TABLE
 CREATE TABLE IF NOT EXISTS teams (
     team_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    noc_id INT,
+    region_id INT,
     team_name VARCHAR(100),
-    FOREIGN KEY (noc_id)
-        REFERENCES noc (noc_id)
+    FOREIGN KEY (region_id)
+        REFERENCES regions (region_id)
 );
