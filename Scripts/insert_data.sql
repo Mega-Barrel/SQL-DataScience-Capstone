f17e7cf645374e3aac9f3f8dc826f1534310e775
Mega-Barrel <saurabhjoshi24business@gmail.com>
2021-02-24 23:22:15 +0530

    Fixed 5 insert statements

--- Scripts/insert_data.sql
+++ Scripts/insert_data.sql
@@ -2,15 +2,53 @@ USE uc_davis;
 
 -- iNSERTING RECORDS IN THE CITIES TABLE
 INSERT INTO cities (city_name)
-SELECT City
+SELECT DISTINCT City
 FROM athlete_events
 GROUP BY City;
 
 
+-- INSERTING RECORDS IN THE MEDALS TABLE
+INSERT INTO medals (medal_type)
+SELECT DISTINCT Medal
+FROM athlete_events
+GROUP BY Medal; 
+
+
 -- INSERTING RECORDS IN THE SEASONS TABLE 
 INSERT INTO seasons (season_name)
-SELECT Season
+SELECT DISTINCT Season
 FROM athlete_events;
+
+
+-- INSERTING RECORDS IN THE GAMES TABLE
+INSERT INTO games ( city_id, season_id, year )
+SELECT c.city_id, s.season_id, a.year
+FROM
+    athlete_events a 
+    INNER JOIN cities c ON a.City = c.city_name
+    INNER JOIN seasons s ON a.Season = s.season_name;
+    
+    
+-- INSERTING RECORDS IN THE SPORTS TABLE
+INSERT INTO sports (season_id, sport_name)
+SELECT s.season_id, a.Sport
+FROM
+	athlete_events a
+	INNER JOIN seasons s ON a.Season = s.season_name;
+
+
+-- INSERTING RECORDS IN THE EVENTS TABLE
+INSERT INTO events (sport_id, event_name)
+SELECT s.sport_id, a.Event
+FROM
+    athlete_events a
+    INNER JOIN sports s ON a.Sport = s.sport_name;
+
+
+-- INSERTING DATA INTO ATHLETES TABLE
+INSERT INTO athletes (region_id, game_id, athlete_name, athlete_gender)
+SELECT g.game_id
+;
 
 
 -- INSERTING RECORDS IN THE REGIONS TABLE
@@ -18,52 +56,12 @@ INSERT INTO regions (city_id, region_name, country_name)
 SELECT c.city_id, co.NOC, co.region
 FROM cities c, country co;
 
--- INSERTING RECORDS IN THE GAMES TABLE
-INSERT INTO games (city_id, season_id, year)
-SELECT (
-    (SELECT city_id FROM cities),
-    (SELECT season_id FROM seasons),
-    (SELECT Year FROM athlete_events)
-);
-
-
--- INSERTING RECORDS IN THE SPORTS TABLE
-INSERT INTO sports (season_id, sport_name)
-SELECT (
-    (SELECT season_id FROM seasons),
-    (SELECT Sport FROM athlete_events)
-);
-
-
--- INSERTING RECORDS IN THE EVENTS TABLE
-INSERT INTO events (sport_id, event_name)
-SELECT (
-    (SELECT sport_id FROM sports),
-    (SELECT Event FROM athlete_events)
-);
-
-
--- INSERTING RECORDS INTO REGIONS TABLE
-INSERT INTO regions (city_id, region_name, country_name)
-SELECT (
-    (SELECT city_id FROM cities),
-    (SELECT NOC, region FROM regions)
-);
-
 
 -- INSERTING RECORDS INTO TEAMS TABLE
 INSERT INTO teams (region_id, team_name)
 SELECT (
     (SELECT region_id FROM regions),
     (SELECT Team FROM athlete_events)
-);
-
-
--- INSERTING DATA INTO ATHLETES TABLE
-INSERT INTO athletes (region_id, athlete_name, athlete_gender)
-SELECT (
-    (SELECT region_id FROM nocs),
-    (SELECT Name, Sex FROM athlete_events)  
 );
 
 
