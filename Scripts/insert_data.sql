f79b56a2cf1ce1cd0e761a2d33b0c1bb68338f66
Mega-Barrel <saurabhjoshi24business@gmail.com>
2021-02-27 00:47:44 +0530

    Modified_query

--- Scripts/insert_data.sql
+++ Scripts/insert_data.sql
@@ -24,9 +24,10 @@ FROM athlete_events;
 INSERT INTO games ( city_id, season_id, year )
 SELECT c.city_id, s.season_id, a.year
 FROM
-    athlete_events a 
+    athlete_events a    
     INNER JOIN cities c ON a.City = c.city_name
-    INNER JOIN seasons s ON a.Season = s.season_name;
+    INNER JOIN seasons s ON a.Season = s.season_name
+GROUP BY c.city_id, s.season_id, a.year;
     
     
 -- INSERTING RECORDS IN THE SPORTS TABLE
@@ -34,43 +35,57 @@ INSERT INTO sports (season_id, sport_name)
 SELECT s.season_id, a.Sport
 FROM
 	athlete_events a
-	INNER JOIN seasons s ON a.Season = s.season_name;
+	INNER JOIN seasons s ON a.Season = s.season_name
+GROUP BY s.season_id, a.Sport;
 
 
 -- INSERTING RECORDS IN THE EVENTS TABLE
 INSERT INTO events (sport_id, event_name)
-SELECT s.sport_id, a.Event
+SELECT DISTINCT s.sport_id, a.Event
 FROM
     athlete_events a
-    INNER JOIN sports s ON a.Sport = s.sport_name;
-
-
--- INSERTING DATA INTO ATHLETES TABLE
-INSERT INTO athletes (region_id, game_id, athlete_name, athlete_gender)
-SELECT g.game_id
-;
+    INNER JOIN sports s ON a.Sport = s.sport_name
+GROUP BY s.sport_id, a.Event;
 
 
 -- INSERTING RECORDS IN THE REGIONS TABLE
 INSERT INTO regions (city_id, region_name, country_name)
-SELECT c.city_id, co.NOC, co.region
-FROM cities c, country co;
+SELECT DISTINCT c.city_id, a.NOC, co.region
+	FROM cities c, athlete_events a
+    INNER JOIN country co ON a.NOC = co.NOC
+GROUP BY c.city_id, a.NOC, co.region;
+
+
+-- INSERTING DATA INTO ATHLETES TABLE
+INSERT INTO athletes (region_id, game_id, athlete_name, athlete_gender)
+SELECT DISTINCT r.region_id, g.game_id, a.Name, a.SEX
+	FROM athlete_events a
+JOIN 	
+	games g ON a.Year = g.year
+JOIN 
+	regions r ON a.NOC = r.region_name
+GROUP by
+	a.Name;
 
 
 -- INSERTING RECORDS INTO TEAMS TABLE
 INSERT INTO teams (region_id, team_name)
-SELECT (
-    (SELECT region_id FROM regions),
-    (SELECT Team FROM athlete_events)
-);
+SELECT DISTINCT r.region_id, a.Team
+	FROM athlete_events a
+    INNER JOIN regions r ON a.NOC = r.region_name
+GROUP BY r.region_id, a.Team;
 
 
 -- INSERTING DATA INTO Sports_Stats TABLE
 INSERT INTO sports_stats (athlete_id, game_id, event_id, medal_id, athlete_age, athlete_height, athlete_weight)
-SELECT (
-    (SELECT athlete_id FROM athletes),
-    (SELECT game_id FROM games),
-    (SELECT event_id FROM events),
-    (SELECT medal_id FROM medals),
-    (SELECT Age, Height, Weight FROM athlete_events)
-);
+SELECT DISTINCT at.athlete_id, g.game_id, e.event_id, m.medal_id, a.Age, a.Height, a.Weight
+	FROM athlete_events a
+JOIN 
+	medals m ON a.Medal = m.medal_type
+JOIN
+	events e ON a.Event = e.event_name
+JOIN
+	games g ON a.Year = g.year
+JOIN
+	athletes at ON a.Sex = at.athlete_gender
+GROUP BY a.Age, a.Height, a.Weight;
